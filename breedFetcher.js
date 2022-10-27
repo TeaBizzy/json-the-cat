dd27980b9dcc93885e89d13171a9ba69b238cec0
Stefan <satalbot@protonmail.com>
2022-10-27 19:58:21 +0000

    Changed all error handling to be done by the callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,21 @@
 const request = require("request");
 
-const args = process.argv.slice(2);
-
 const fetchBreedDescription = function(breed, callback) {
   request.get(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
     if (error) {
-      throw (error);
+      callback(error, null);
+      process.exit();
     }
-  
+
     let data = JSON.parse(body); // Have to parse the data first to check if this was a bad inquiry.
 
     // Check for a bad inquiry,
     if (data.length < 1) {
       const invalidInquiryErr = new Error(`Invalid inquiry: ${breed}. Please try again...`);
-      throw (invalidInquiryErr);
+      callback(invalidInquiryErr, null);
     }
 
-    callback(error, data[0].description);
+    callback(null, data[0].description);
   });
 };
 
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const { fetchBreedDescription } = require("./breedFetcher");
 
+const args = process.argv.slice(2);
+const breedName = args[0];
 
-fetchBreedDescription("Bengal", (error, description) => {
-  console.log(description);
+fetchBreedDescription(breedName, (error, description) => {
+  if (error) {
+    console.log("Error fetch details:", error);
+  } else {
+    console.log(description);
+  }
 });
