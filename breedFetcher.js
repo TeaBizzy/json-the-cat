6f5648ee78df7bbe16efac0bfb2fff714c73a6cd
Stefan <satalbot@protonmail.com>
2022-10-27 19:16:12 +0000

    Fetches the breed description of the given argument

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,18 @@
 const request = require("request");
+
+const args = process.argv.slice(2);
+const breed = args[0];
+
+request.get(`https://api.thetapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  if (error) {
+    throw (error);
+  }
+
+  let data = JSON.parse(body); // Have to parse the data first to check if this was a bad inquiry.
+
+  // Check for a bad inquiry,
+  if (data.length < 1) {
+    const invalidInquiryErr = new Error(`Invalid inquiry: ${breed}. Please try again...`);
+    throw (invalidInquiryErr);
+  }
+});
